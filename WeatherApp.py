8170fbae6d39918c25402a4f9ae9d60db25b08d6
Zach <mcbreartyzg@gmail.com>
2018-07-27 22:40:28 +0100

    compatability with new RSS API from BBC, quite slow for some reason :(

--- WeatherApp.py
+++ WeatherApp.py
@@ -1,12 +1,12 @@
 # Weather App, using the bbc rss feed
-# feed: http://open.live.bbc.co.uk/weather/feeds/en/num/3dayforecast.rss
-# the number after /en/ is the area code, it can be changed for different areas
+#
 import feedparser # https://github.com/kurtmckee/feedparser
 import tkinter as tk # inbuilt
-num = 0
-url = "http://open.live.bbc.co.uk/weather/feeds/en/{}/3dayforecast.rss".format(num)
 
-#feedData = parseData(feed)
+#~6 digit code for each area
+#found by first going to website and copying from url
+num = 000000 
+url = f"https://weather-broker-cdn.api.bbci.co.uk/en/forecast/rss/3day/{num}"
 
 def parseData(feed): # feed will be the parsed weather url
     _3DayData = feed["entries"] # the days are stored in entries
@@ -19,15 +19,18 @@ def parseData(feed): # feed will be the parsed weather url
     return _3DayDataFull
 
 def printDisplay(data): # basic display in idle or terminal
-    for x in data: #_3DayDataFull:
+    for x in data: # eg _3DayDataFull:
         print(x[0]) # dayName
         for y in x[1]:
-            print(y) # all the data in daySummary
+            print("-", y) # all the data in daySummary
         print() # separates each day 
 
 def showTkinter(data, root=None): # tkinter to display the data, days in blue
     if root == None:
         root = tk.Tk()
+    # required by terms of use>
+    # THINK: highlight in red?
+    tk.Label(root, text="BBC Weather Unofficial").pack(anchor="n")
     for x in data:
         #dayName, highlighted in blue
         tk.Label(root, text=x[0], bg="skyblue").pack(anchor="w", expand=1)
@@ -38,17 +41,18 @@ def showTkinter(data, root=None): # tkinter to display the data, days in blue
         tk.Label(root, text="").pack(anchor="w")
     root.mainloop()
 
-def update(root):
+def update(root, TESTING=False):
     feed = feedparser.parse(url)
-    showTkinter(parseData(feed),root)
+    if not TESTING:
+        showTkinter(parseData(feed),root)
+    else:
+        printDisplay(parseData(feed))
 
 if __name__=="__main__":
     root = tk.Tk()
     root.resizable(width=False, height=False)
-    root.title("South Benfleet Weather")
+    root.title("Weather")
     root.geometry(newGeometry="300x800+150+150")
     update(root)
-    tk.Button(root,text="Update", command=lambda: update(root)).pack(side=tk.Bottom)
+        
     
-    
-##    printDisplay(parseData(feed))
